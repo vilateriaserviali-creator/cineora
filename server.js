47889b10233462c2e8d57c9dd35c30f3520158a5
vilateriaserviali-creator <vilateriaserviali@gmail.com>
2026-09-22 12:05:16 +0600

    Исправление запуска CINEORA

--- server.js
+++ server.js
@@ -138,4 +138,6 @@ io.on("connection", socket => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`CINEORA running on port ${PORT}`));
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`CINEORA running on port ${PORT}`);
+});
